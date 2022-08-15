30bd570dc4c116ae9807ff49f7d0bbc409bc5e54
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-08-14 23:53:41 -0400

    validation package added and implemented server side.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const { genSalt, hash } = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +15,29 @@ const userSchema = new Schema({
     required: true,
   },
 })
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password)
+    throw Error('Missing fields, all fields must be filled!')
+  if (!validator.isEmail(email)) throw Error('Invalid email!')
+  if (!validator.isStrongPassword(password))
+    throw Error('Password not strong enough!')
+
+  const exists = await this.findOne({ email })
+
+  if (exists) throw Error('Email is already in use.')
+
+  const salt = await genSalt(10)
+  const hashed = await hash(password, salt)
+
+  const user = await this.create({
+    email,
+    password: hashed,
+  })
+
+  return user
+}
 
 module.exports = mongoose.model('User', userSchema)
